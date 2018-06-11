c5f20d1995979df7e85cce313511f618aa505a18
Kirill Alekseev <kirill.alekseev@corp.mail.ru>
2018-06-12 02:59:16 +0300

    http: requests validation

--- src/technochat/http/messageview.go
+++ src/technochat/http/messageview.go
@@ -1,9 +1,55 @@
 package http
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func (s *Server) messageView(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented", http.StatusNotImplemented)
+type MessageViewRequest struct {
+	method string
+
+	id string
+}
+
+type MessageViewResponse struct {
+	Text string `json:text`
+}
+
+func NewMessageViewRequest(r *http.Request) (*MessageViewRequest, error) {
+	req := &MessageViewRequest{}
+
+	if err := r.ParseForm(); err != nil {
+		return nil, err
+	}
+
+	req.method = r.Method
+	req.id = r.PostFormValue("id")
+
+	return req, nil
+}
+
+func (req *MessageViewRequest) Validate() error {
+	if req.method != "GET" {
+		return fmt.Errorf("GET required")
+	}
+
+	if len(req.id) == 0 {
+		return fmt.Errorf("empty id")
+	}
+
+	return nil
+}
+
+// TODO
+func (s *Server) messageView(r *http.Request) (int, interface{}, error) {
+	req, err := NewMessageViewRequest(r)
+	if err != nil {
+		return http.StatusBadRequest, nil, err
+	}
+
+	if err := req.Validate(); err != nil {
+		return http.StatusBadRequest, nil, err
+	}
+
+	return http.StatusNotImplemented, nil, err
 }
--- src/technochat/http/server.go
+++ src/technochat/http/server.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
@@ -8,6 +9,14 @@ import (
 type Server struct {
 	addr string
 }
+
+type Response struct {
+	Code  int         `json:code,omitempty`
+	Error string      `json:error,omitempty`
+	Body  interface{} `json:body,omitempty`
+}
+
+type TechnochatHandler func(*http.Request) (int, interface{}, error)
 
 func NewServer(addr string) *Server {
 	return &Server{
@@ -18,10 +27,10 @@ func NewServer(addr string) *Server {
 func (s *Server) Init() {
 	log.Println("http: initialising")
 
-	http.HandleFunc("/api/v1/message/add", s.messageAdd)
-
 	http.HandleFunc("/", s.index)
-	http.HandleFunc("/message/view", s.messageView)
+
+	http.HandleFunc("/api/v1/message/add", respond(s.messageAdd))
+	http.HandleFunc("/api/v1/message/view", respond(s.messageView))
 
 	log.Println("http: successfully initialised")
 }
@@ -32,4 +41,46 @@ func (s *Server) Routine() {
 
 func (s *Server) index(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/static/index.html", http.StatusMovedPermanently)
+}
+
+func getRealRemoteAddr(r *http.Request) string {
+	if xRealIp := r.Header.Get("X-Real-Ip"); xRealIp != "" {
+		return xRealIp
+	} else {
+		return r.RemoteAddr
+	}
+}
+
+func respond(h TechnochatHandler) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		api := r.URL.EscapedPath()
+		remoteAddr := getRealRemoteAddr(r)
+
+		var (
+			resp Response
+			err  error
+		)
+
+		w.Header().Add("Content-Type", "application/json")
+
+		resp.Code, resp.Body, err = h(r)
+		if err != nil {
+			switch resp.Code {
+			case http.StatusBadRequest:
+				log.Printf("%s: bad request from %s: %v\n", api, remoteAddr, err)
+				resp.Body = err.Error()
+			case http.StatusForbidden:
+				log.Printf("%s: forbidden for %s: %v\n", api, remoteAddr, err)
+				resp.Body = err.Error()
+			case http.StatusInternalServerError:
+				log.Printf("%s: internal server error for %s: %v\n", api, remoteAddr, err)
+				resp.Body = http.StatusText(resp.Code)
+			default:
+				resp.Body = http.StatusText(resp.Code)
+			}
+		}
+
+		respMarshalled, _ := json.Marshal(resp)
+		w.Write(respMarshalled)
+	}
 }
