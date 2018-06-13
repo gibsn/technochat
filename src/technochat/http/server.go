12dba4f785de68412378aece643c422eec48ef67
Kirill Alekseev <kirill.alekseev@corp.mail.ru>
2018-06-13 03:53:21 +0300

    static: ajax request to /message/add

--- src/technochat/http/server.go
+++ src/technochat/http/server.go
@@ -22,9 +22,8 @@ type Server struct {
 }
 
 type Response struct {
-	Code  int         `json:"code,omitempty"`
-	Error string      `json:"error,omitempty"`
-	Body  interface{} `json:"body,omitempty"`
+	Code int         `json:"code,omitempty"`
+	Body interface{} `json:"body,omitempty"`
 }
 
 type TechnochatHandler func(*http.Request) (int, interface{}, error)
