aae0aa337f1115abab8e0db1d26f51da5d28adc9
Kirill Alekseev <kirill.alekseev@corp.mail.ru>
2018-06-20 23:58:29 +0300

    added deploy script for Windows

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+# generating selfsigned ssl certificates
+if (!(Test-Path ./certs/server.key) -or -!(Test-Path ./certs/server.crt)) {
+    mkdir certs
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=US" -keyout ./certs/server.key -out ./certs/server.crt
+}
+
+docker-compose -f dist/docker-compose.yml build
+docker-compose -f dist/docker-compose.yml down
+docker-compose -f dist/docker-compose.yml up -d
+
+echo "done"
