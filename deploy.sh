da6dab89a05732720ae4477e2c6c591fc50da3d3
Kirill Alekseev <kirill.alekseev@corp.mail.ru>
2020-04-18 14:57:47 +0300

    deployment: adding versions to static files links on build #55 (#56)

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+DOCKER_COMPOSE_CFG="dist/docker-compose.yml"
+
+ARGS=("$@")
+
+
+print_help() {
+    echo "$0 builds required docker images and restarts the running containers"
+    echo -e "\t--dev:\trun in developer mode"
+    echo -e "\t--help:\tprints this text and exits"
+}
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        "--dev") DOCKER_COMPOSE_CFG="dist/docker-compose-dev.yml";;
+        "--help") print_help; exit 0;;
+    esac
+    shift
+done
+
 # generating selfsigned ssl certificates
 if [ ! -f certs/server.key ] || [ ! -f certs/server.crt ]; then
     mkdir certs
@@ -7,8 +27,8 @@ if [ ! -f certs/server.key ] || [ ! -f certs/server.crt ]; then
         -keyout certs/server.key -out certs/server.crt || exit
 fi
 
-docker-compose -f dist/docker-compose.yml build  || exit
-docker-compose -f dist/docker-compose.yml down   || exit
-docker-compose -f dist/docker-compose.yml up -d  || exit
+docker-compose -f $DOCKER_COMPOSE_CFG build  || exit
+docker-compose -f $DOCKER_COMPOSE_CFG down   || exit
+docker-compose -f $DOCKER_COMPOSE_CFG up -d  || exit
 
 echo "done"
