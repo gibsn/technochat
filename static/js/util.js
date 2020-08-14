31d3767b21e9627bf76345bb3fc6cb94c11af7c2
Aleksandr Shkardun <slipnir@yandex.ru>
2020-08-14 17:35:38 +0300

    symbol counter - rework

--- static/js/util.js
+++ static/js/util.js
@@ -42,35 +42,3 @@ function copyToClipboardAny(id) {
 
     return isCopied
 }
-
-// symbols counter
-const maxLength = 4;
-
-var textarea = document.querySelector('.js__textarea');
-var counter = document.querySelector('.js__counter');
-var counterMax = document.querySelector('.js__counter-max');
-
-counterMax.innerHTML = maxLength;
-textarea.setAttribute('maxlength', maxLength);
-
-textarea.addEventListener('input', function () {
-    var currentLength = textarea.value.length;
-    if (currentLength > maxLength) { // если строго >, то красный цвет не добавляется, если >=, то красный добавляется, но последняя цифра не меняется
-        counter.parentElement.style.color = 'red';
-    } else {
-        counter.innerHTML = currentLength;
-        counter.parentElement.style.color = '#6d6d6d';
-    }
-});
-
-console.log(counter.parentElement);
-
-// $('#text').on('input', function () {
-//     var maxL = $(this).attr('maxlength');
-//     var currL = $(this).val().length;
-//     if (currL > maxL) {
-//         alert('Too many symbols');
-//     } else {
-//         $('#text-length').text(currL);
-//     }
-// });
