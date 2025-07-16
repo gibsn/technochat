3f10a77465c654ccb6d586772c64dbb4f12d68e5
Alex Shkardun <slipnir@yandex.ru>
2025-07-16 21:39:23 +0300

    copy button refactoring - added logs

--- static/js/util.js
+++ static/js/util.js
@@ -1,22 +1,33 @@
 export function copyButton(button, input) {
     const copyButton = document.getElementById(button);
 
-    if (copyButton) {
-        copyButton.addEventListener('click', async () => {
-            const targetInput = document.getElementById(input);
-
-            if (targetInput) {
-                const inputValue = targetInput.value;
-
-                try {
-                    await navigator.clipboard.writeText(inputValue);
-                    copyButton.textContent = 'Copied!';
-                } catch (err) {
-                    console.warn(err);
-                }
-            }
-        });
+    if (!copyButton) {
+        console.error('Copy button was not found');
+        return;
     }
+
+    copyButton.addEventListener('click', async () => {
+        const targetInput = document.getElementById(input);
+
+        if (!targetInput) {
+            console.error('Copy block was not found');
+            return;
+        }
+
+        const inputValue = targetInput.value;
+
+        if (!inputValue.trim()) {
+            console.error('Copy link was not found');
+            return;
+        }
+
+        try {
+            await navigator.clipboard.writeText(inputValue);
+            copyButton.textContent = 'Copied!';
+        } catch (err) {
+            console.error('Failed to copy:', err);
+        }
+    });
 }
 
 export function scrollToCopyButton() {
